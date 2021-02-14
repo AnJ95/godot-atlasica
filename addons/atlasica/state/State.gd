865b74c880fdf2279032e674facafc98d78d66ec
Angelo Aracri <angeloaracri@yahoo.de>
2021-02-14 15:07:14 +0100

    Opening zip with the power of gdunzip

--- addons/atlasica/state/State.gd
+++ addons/atlasica/state/State.gd
@@ -3,14 +3,31 @@ extends Resource
 
 signal state_changed()
 
+export(String) var path_zip = null setget _set_path_zip
+
 export(String) var path_spritesheet_image = null setget _set_path_spritesheet_image
 export(String) var path_spritesheet_data = null setget _set_path_spritesheet_data
 
-
-
 func is_valid():
 	return has_valid_spritesheet_image() and has_valid_spritesheet_data()
+
+func has_valid_zip_path():
+	return Directory.new().file_exists(path_zip)
+func has_valid_zip(get_result=false):
+	var f:File = File.new()
 	
+	# Instance the gdunzip script
+	var gdunzip = load("res://addons/atlasica/scripts/gdunzip.gd").new()
+
+	# Check if zip can be opened and if files are present
+	if !gdunzip.load(path_zip):
+		return null if get_result else false
+	if !gdunzip.files.has("layout.json"):
+		return null if get_result else false
+	if !gdunzip.files.has("spritesheet.png"):
+		return null if get_result else false
+	
+	return gdunzip if get_result else true
 
 func has_configured_spritesheet_image_path():
 	return path_spritesheet_image != null
@@ -59,4 +76,8 @@ func _set_path_spritesheet_image(v):
 
 func _set_path_spritesheet_data(v):
 	path_spritesheet_data = v
+	emit_signal("state_changed")
+
+func _set_path_zip(v):
+	path_zip = v
 	emit_signal("state_changed")
