1cca43b14e1b9e91a61fb51f5583122b5e914611
Angelo Aracri <angeloaracri@yahoo.de>
2021-02-14 21:12:12 +0100

    Display metadata in inspector

--- addons/atlasica/dock/Inspector.gd
+++ addons/atlasica/dock/Inspector.gd
@@ -12,6 +12,7 @@ onready var lblName:Label = $VBoxContainer/LblName
 onready var lblPos:Label = $VBoxContainer/HBoxContainer/LblPos
 onready var lblSize:Label = $VBoxContainer/HBoxContainer2/LblSize
 onready var textureRect:TextureRect = $VBoxContainer/TextureRect
+onready var lblMetadata:Label = $VBoxContainer/VBoxContainer/PanelContainer/lblMetadata
 
 func _on_item_hovered(item_name, item):
 	cur_item_name = item_name
@@ -19,6 +20,7 @@ func _on_item_hovered(item_name, item):
 	lblName.text = item_name
 	lblPos.text = "(%d, %d)" % [item.x, item.y]
 	lblSize.text = "(%d, %d)" % [item.w, item.h]
+	lblMetadata.text = JSON.print(item.metadata, "  ")
 	
 	if preview_sprite:
 		textureRect.texture = Atlasica.get_sprite(item_name)
@@ -30,7 +32,7 @@ func _on_item_unhovered(_item_name, _item):
 	cur_item_name = null
 	modulate.a = 0
 
-
 func _on_TextureRect_resized():
+	var textureRect:TextureRect = $VBoxContainer/TextureRect
 	textureRect.rect_min_size.y = textureRect.rect_size.x
 	textureRect.rect_size.y = textureRect.rect_size.x
